0e6f24e4c501d35af57931635315be583e88d1d7
Walker Lobato <walker.b.lobato@gmail.com>
2022-09-23 15:42:27 -0300

    Add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,13 +1,33 @@
-import { Link } from 'react-router-dom';
+import { useState } from "react"
+
  
 export const Login = () => {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEnter = () => {
+        console.log(email);
+        console.log(password)
+    }
     return (
         <div>
-            Login
-            <Link to={'/pagina-inicial'}>
-                <button >Página inicia</button>
-            </Link>
+            <form>
+
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+
+                <button type="button" onClick={handleEnter}>
+                    Entrar
+                </button>
+            </form>
         </div>
     )
 }
