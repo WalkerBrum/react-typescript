2c0f59c81f6e5ed5e35a5c636a315dcf4028e208
Walker Lobato <walker.b.lobato@gmail.com>
2022-09-30 13:06:54 -0300

    feat: add hooks

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,23 @@
 import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 export const Dashboard = () => {
+    // useRef pode ser usado para alterar algum valor por um tipo de ação do usuário, mas não quer fazer um re-render
+    const counterRef = useRef({
+        counter: 0
+    });
+    /*ou
+      const counterRef = useRef(0);
+      usar assim
+      counterRef.current
+    */
+
     return (
         <div>
             <p>Dashboard</p>
+            <p>Contador: {counterRef.current.counter}</p>
+            <button onClick={() => counterRef.current.counter++}>Somar</button>
+            <button onClick={() => console.log(counterRef.current.counter)}>Logar</button>
             <Link to="/entrar">Login</Link>
         </div>
     )
--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,10 +1,19 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useMemo, useState, useRef } from "react"
 
  
 export const Login = () => {
 
+    // useRef consegue armazenar um valor que pode ser alterado caso o desenvolvedor queira.
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    // useMemo é usado para evitar que o código com cálculos seja refeito toda vez que um estado dentro da aplicação seja alterada, então só será refeito quando algo dentro desse código for modificado.
+    const emailLength = useMemo(() => {
+        console.log('Executou')
+        return email.length * 1000;
+    }, [email.length]);
 
     useEffect(() => {
         if (window.confirm('Você é homem?')) return console.log('Homem');
@@ -20,27 +29,43 @@ export const Login = () => {
         console.log(password);
     }, [password]);
 
-    const handleEnter = () => {
+    // useCallback evita que função seja reconstruída toda vez que um state do component seja alterado, é parecido com useNemo, porém usado mais para funções.
+    const handleEnter = useCallback(() => {
         console.log(email);
-        console.log(password)
-    }
+        console.log(password);
+    }, [email, password]);
+
     return (
         <div>
             <form>
 
+                <p>Quantidade de caracteres no email: {emailLength}</p>
+
                 <label>
                     <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        // Lógica quando aperta tecla enter
+                        onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <input 
+                        type="password" 
+                        value={password} 
+                        ref={inputPasswordRef}
+                        onChange={e => setPassword(e.target.value)}/>
                 </label>
 
-                <button type="button" onClick={handleEnter}>
-                    Entrar
+                <button 
+                    type="button" 
+                    onClick={handleEnter}>
+                        Entrar
                 </button>
+
             </form>
         </div>
     )
